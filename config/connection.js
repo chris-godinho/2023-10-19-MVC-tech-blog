06c2c52fd6ce47c316ea6efd16862889f440ff08
kristbg <kristbg@gmail.com>
2023-10-28 19:48:22 -0400

    Fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,28 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+if (process.env.DATABASE_URL) {
+  const { hostname, port, username, password, pathname } = new URL(process.env.DATABASE_URL);
+  sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: hostname,
+    port: port,
+    username: username,
+    password: password,
+    database: pathname.substring(1),
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
+
+/*
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,5 +33,6 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
+*/
 
 module.exports = sequelize;
