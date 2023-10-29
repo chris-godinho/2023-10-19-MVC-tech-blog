307f56b4ad0f2c3c850d190100558092c7f6af3a
kristbg <kristbg@gmail.com>
2023-10-28 20:48:22 -0400

    Fixed logout route

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,5 @@
 const logout = async () => {
+    console.info("LOGOUT CLICKED");
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
